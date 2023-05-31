b25c377ac1bc59e5ba0091d3619d991fac06faae
Kevin Kibe <kchegz234@gmail.com>
2023-05-31 16:48:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-
+from PyPDF2 import PdfReader
+from langchain.document_loaders import PyPDFLoader
+from langchain.vectorstores import Chroma
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import Docx2txtLoader
+from langchain.document_loaders import TextLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS 
+from docx2pdf import convert
+import os
+import sys
